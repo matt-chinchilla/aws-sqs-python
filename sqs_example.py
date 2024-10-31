c010bac738138d02f9cd4d6b398c18bb4fee3fe5
matt-chinchilla <70705923+matt-chinchilla@users.noreply.github.com>
2024-10-31 14:03:03 -0400

    Add receive_messages function to process and delete messages from SQS queue

--- sqs_example.py
+++ sqs_example.py
@@ -1,5 +1,6 @@
 # Load the AWS SDK for Python
 import boto3
+import time
 
 # Load the exceptions for error handling
 from botocore.exceptions import ClientError, ParamValidationError
@@ -79,13 +80,53 @@ def create_messages(queue_url):
             print("Parameter validation error: %s" % e)
         except ClientError as e:
             print("Client error: %s" % e)
-        
+
+## Create Receive Messages Function in order to Process them
+def receive_messages(queue_url):
+    print('Reading messages')
+    while True: # Loops through Continuously // If no ==> time.sleep(60)
+        try:
+            data = sqs.receive_message( # Dictionary defined in the AWS API Documentation
+                QueueUrl = queue_url,
+                MaxNumberOfMessages = 10,
+                VisibilityTimeout = 60,
+                WaitTimeSeconds = 20
+            )
+        # An error occurred
+        except ParamValidationError as e:
+            print("Parameter validation error: %s" % e)
+        except ClientError as e:
+            print("Client error: %s" % e)
+        else:
+            # Check if empty receive
+            try:
+                data['Messages']
+            except KeyError:
+                data = None
+            if data is None:
+                print('Queue empty waiting 60s')
+                # Wait for 60 seconds
+                time.sleep(60)
+            else:
+                for message in data['Messages']:
+                    print(message)
+                    sqs.delete_message(
+                        QueueUrl=queue_url,
+                        ReceiptHandle=message['ReceiptHandle']
+                    )
+                    print('Deleted message')
+                    # Wait for 1 second
+                    # time.sleep(1)
+            # Do not forget to add this call to the main program
+            # Gotta process them and delete from the queue now
+ 
 # Main program
 def main():
     sqs_queue_url = create_sqs_queue('backspace-lab')
     print('Successfully created SQS queue URL '+ sqs_queue_url )
     create_messages(sqs_queue_url)
     print("Successfully sent 50 messages to the SQS queue")
+    receive_messages(sqs_queue_url)
 
 if __name__ == '__main__':
     main()
